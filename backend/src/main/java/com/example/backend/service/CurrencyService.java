20879e7bb4a6c3daf0e8882f85550ac4461044e8
Jordan Tanti <jordantanti@gmail.com>
2023-10-08 18:58:48 +1100

    added error handling and front end API

--- backend/src/main/java/com/example/backend/service/CurrencyService.java
+++ backend/src/main/java/com/example/backend/service/CurrencyService.java
@@ -2,11 +2,10 @@ package com.example.backend.service;/*
 Handle communication between openexchangerates
  */
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 
 
 @Service
@@ -17,14 +16,23 @@ public class CurrencyService {
         this.restTemplate = restTemplate;
     }
 
-    public String getExchangeRates() {
+    public String getExchangeRates() throws ResourceAccessException {
         String API_URL = "https://openexchangerates.org/api/latest.json?app_id=371446c007bb4489a465d02220f4d516&base=USD&callback=someCallbackFunction";
-        String response = restTemplate.getForObject(API_URL, String.class);
+
+        String response;
+        try {
+            response = restTemplate.getForObject(API_URL, String.class);
+        } catch(ResourceAccessException e) {
+            System.out.println("Problem accessing the exchange rate API. Check internet connection.");
+            return "";
+        }
+
 
         if(response != null) {
             response = response.replace("someCallbackFunction(", "");
             response = response.replace(")", "");
         }
-        return response;z 
+
+        return response;
     }
 }
