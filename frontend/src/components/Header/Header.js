a65c0b4617279f45f961db14746c552b873577db
dinhluong1402 <luongnguyendinh1999@gmail.com>
2023-10-11 01:00:56 +1100

    Update frontend

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,46 +1,9 @@
-import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
-import Dropdown from '@mui/joy/Dropdown';
-import IconButton from '@mui/joy/IconButton';
-import Menu from '@mui/joy/Menu';
-import MenuButton from '@mui/joy/MenuButton';
-import MenuItem from '@mui/joy/MenuItem';
 import Typography from '@mui/joy/Typography';
 import Link from '@mui/joy/Link';
-import { Link as RouterLink, useLocation, useNavigate } from 'react-router-dom';
-import { cleanToken, getRefreshToken } from '../../utils/token';
-import { toast } from 'react-toastify';
-import apiInstance from '../../apis/Axios';
-
-const publicRoutes = [
-  '/login',
-  '/signup',
-  '/forgot-password',
-  '/reset-password',
-];
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function Header() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const isPublicPage = publicRoutes.includes(location.pathname);
-  const refreshToken = getRefreshToken();
-
-  const handleLogout = async () => {
-    const response = await apiInstance
-      .post('/auth/logout', { refreshToken })
-      .then(response => {
-        toast.success('Successfully logout!');
-        cleanToken();
-        navigate('/login');
-        return true;
-      })
-      .catch(error => {
-        console.log('logout error: ', error.response);
-        toast.error('Something wrong with your account.');
-        return false;
-      });
-    return response;
-  };
   return (
     <Box
       component="header"
@@ -84,41 +47,6 @@ export default function Header() {
           Financial Application
         </Typography>
       </Link>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '1rem',
-        }}
-      >
-        {!isPublicPage && (
-          <Dropdown>
-            <MenuButton
-              slots={{ root: IconButton }}
-              slotProps={{ root: { variant: 'plain', color: 'neutral' } }}
-              sx={{ borderRadius: 50, padding: 0 }}
-            >
-              <Avatar>NL</Avatar>
-            </MenuButton>
-            <Menu>
-              <MenuItem>
-                <Link
-                  sx={{
-                    '&:hover': {
-                      textDecorationLine: 'none',
-                    },
-                  }}
-                  component={RouterLink}
-                  to="/profile"
-                >
-                  Profile
-                </Link>
-              </MenuItem>
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </Menu>
-          </Dropdown>
-        )}
-      </Box>
     </Box>
   );
 }
