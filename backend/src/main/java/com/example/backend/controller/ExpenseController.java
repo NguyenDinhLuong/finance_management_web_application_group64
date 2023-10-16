5bd223e432bef53d30aee001d4d69294b99945e0
Jordan Tanti <jordantanti@gmail.com>
2023-10-16 21:35:21 +1100

    added expense table

--- backend/src/main/java/com/example/backend/controller/ExpenseController.java
+++ backend/src/main/java/com/example/backend/controller/ExpenseController.java
@@ -1,10 +1,8 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.NormalExpense;
-import com.example.backend.model.RecurringExpense;
 import com.example.backend.security.services.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/main/java/com/example/backend/model/NormalExpense.java
+++ backend/src/main/java/com/example/backend/model/NormalExpense.java
@@ -17,20 +17,30 @@ public class NormalExpense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private Double amount;
+    @Column(nullable = false)
     private String description;
+    @Column(nullable = false)
     private String category;
-    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(pattern = "yyyy-MM-dd") // changed to match LocalDate format
+    @Column(nullable = false)
     private Date date;
+    @Column(nullable = true)
+    private Integer frequency;
+    @Column(nullable = true)
+    private Date endDate;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 
-    public NormalExpense(Double amount, String description, String category, Date date) {
+    public NormalExpense(Double amount, String description, String category, Date date, Integer frequency, Date endDate) {
         this.amount = amount;
         this.description = description;
         this.category = category;
         this.date = date;
+        this.frequency = frequency;
+        this.endDate = endDate;
     }
 }
